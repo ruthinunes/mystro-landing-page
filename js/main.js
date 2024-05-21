0834b7a00e1a401d7e64157a9d67a4e30842d0a9
ruthinunes <pamelanunesdev@gmail.com>
2024-05-21 19:17:03 -0300

    set up automatically footer year and update js functions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
-const collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach((item) =>
-  item.addEventListener("click", function () {
-    this.classList.toggle("collapsible--expanded");
-  })
-);
+// collapsible contents
+const setCollapsible = () => {
+  const collapsibles = document.querySelectorAll(".collapsible");
+  collapsibles.forEach((item) =>
+    item.addEventListener("click", () => {
+      item.classList.toggle("collapsible--expanded");
+    })
+  );
+};
+
+// automatically update footer year
+const setFooterYear = () => {
+  const year = new Date().getFullYear();
+  document.getElementById("footer-year").innerHTML = year;
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  setCollapsible();
+  setFooterYear();
+});
